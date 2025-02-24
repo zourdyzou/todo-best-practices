17c495960238b0e1a0fa097aa25366a1cb171f20
Zourdy M <zourdy@bereyziat.dev>
2025-02-24 11:27:27 +0100

    feat: implement caching startegy for get request response

--- src/api/apiHooks/useUsers.ts
+++ src/api/apiHooks/useUsers.ts
@@ -10,6 +10,8 @@ export const useUsers = (limit: number = 10) => {
       return nextPage < lastPage.total ? nextPage : undefined;
     },
     initialPageParam: 0,
+    staleTime: 1000 * 60 * 10, // Data considered fresh for 15 minutes
+    gcTime: 1000 * 60 * 30, // Cache persists for 30 minutes before garbage collection
   });
 
   const users = query.data?.pages.flatMap(page => page.users) ?? [];
